dd2ece038d999e54e8478fd9b83fa858e1925a89
Diego Pacori <dpacoria@unsa.edu.pe>
2023-11-07 20:31:53 -0500

    Se realizo correccion en las animaciones y el cambio de color

--- src/auth/components/Register.tsx
+++ src/auth/components/Register.tsx
@@ -6,7 +6,7 @@ export const Register = () => {
     return (
         <div className="flex flex-col items-center justify-center w-full h-full gap-4 ">
             <p className="text-5xl font-normal font-russo-one text-slate-50">Sign Up</p>
-            <form className="flex flex-col items-center w-full gap-3 ">
+            <form className="flex flex-col items-center w-full gap-8 ">
                 <Input title='Name' type='text'/>
                 <Input title='Email' type='email'/>
                 <Input title='Password' type='password'/>
--- src/auth/pages/AuthPanel.tsx
+++ src/auth/pages/AuthPanel.tsx
@@ -7,27 +7,33 @@ export const AuthPanel = () => {
 
     const [isLogin, setIsLogin] = useState<boolean>(true);
     const toggleLogin = () => {
-        setIsLogin(l => !l);
+        setIsLogin(!isLogin);
     };
 
 
     return (
         <>
-            <div className="flex items-center justify-center w-screen h-screen bg-electric-violet-100">
-                <div className="relative flex flex-col items-center w-4/5 rounded-sm md:flex-row h-4/5">
-                    <div className={`w-full flex flex-row relative h-full transition-all duration-1000 p-7 md:${isLogin ? 'w-2/5' : 'w-3/5'} bg-electric-violet-200`}>
+            <div className="flex items-center justify-center w-screen h-full min-h-screen px-12 py-12 bg-electric-violet-100">
+                <div className="relative flex flex-col items-center w-full rounded-sm md:flex-row min-h-max h-[700px]">
+                    <div className={`w-full flex flex-row justify-center relative h-full transition-all duration-1000 p-7 ${isLogin ? 'md:w-2/5 bg-electric-violet-200' : 'md:w-3/5 bg-electric-violet-700'} bg-electric-violet-200`}>
                         <div className={`flex transition-all duration-700 ease-in-out flex-col ${isLogin ? 'opacity-100' : 'w-0 -translate-x-48 overflow-hidden'} items-center justify-center h-full gap-9`}>
                             <p className='text-5xl font-bold tracking-wider text-center font-russo-one text-electric-violet-800'>Hello, Friend!</p>
                             <p className='text-base font-bold tracking-wider text-center font-russo-one text-electric-violet-500'>Enter your personal details and start journey with us</p>
                             <Button fn={toggleLogin} title='Sign Up' px='px-9' py='py-4'rounded='rounded-full'/>
                         </div>
-                        <div className={`transition-all duration-700 ease-in-out overflow-hidden ${isLogin ? 'w-0' : 'opacity-100'} w-full`}>
+                        <div className={`transition-all duration-700 ease-in-out translate-x-0 ${isLogin ? 'translate-x-14 opacity-0 z-0 w-0 overflow-hidden' : 'opacity-100 w-full h-full'}`}>
                             <Register />
                         </div>
                     </div>
-                    <div className={`w-full h-full py-5 transition-all duration-1000 md:${isLogin ? 'w-3/5' : 'w-2/5'} bg-electric-violet-700`}>
-                        <Login />
-                        <Button fn={toggleLogin} title='Sign Up' px='px-9' py='py-4'rounded='rounded-full'/>
+                    <div className={`w-full flex flex-row justify-center relative h-full transition-all duration-1000 p-7 ${isLogin ? 'md:w-3/5 bg-electric-violet-700' : 'md:w-2/5 bg-electric-violet-200'}`}>
+                        <div className={`flex transition-all duration-700 ease-in-out flex-col ${isLogin ? 'w-0 translate-x-48 overflow-hidden' : 'opacity-100'} items-center justify-center h-full gap-9`}>
+                            <p className='text-5xl font-bold tracking-wider text-center font-russo-one text-electric-violet-800'>Hello, Friend!</p>
+                            <p className='text-base font-bold tracking-wider text-center font-russo-one text-electric-violet-500'>Enter your personal details and start journey with us</p>
+                            <Button fn={toggleLogin} title='Sign Up' px='px-9' py='py-4'rounded='rounded-full'/>
+                        </div>
+                        <div className={`transition-all duration-700 ease-in-out translate-x-0 ${isLogin ? 'opacity-100 w-full h-full' : 'translate-x-14 opacity-0 z-0 w-0 overflow-hidden'}`}>
+                            <Login />
+                        </div>
                     </div>
                 </div>
             </div>
